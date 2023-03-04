2f9c2d6160308cdae8aa0574d651d794b79adb1b
thilankas961 <thilankas961@gmail.com>
2023-03-04 12:03:16 +0530

    signup created

---
+++ lib/screens/register/register.dart
@@ -0,0 +1,127 @@
+import 'package:flutter/material.dart';
+import 'package:madhack_finals/Screens/login/login.dart';
+import 'package:madhack_finals/components/background.dart';
+
+class RegisterScreen extends StatelessWidget {
+  const RegisterScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+
+    return Scaffold(
+      body: Background(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              alignment: Alignment.centerLeft,
+              padding: EdgeInsets.symmetric(horizontal: 40),
+              child: Text(
+                "REGISTER",
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Color(0xFF2661FA),
+                    fontSize: 36
+                ),
+                textAlign: TextAlign.left,
+              ),
+            ),
+
+            SizedBox(height: size.height * 0.03),
+
+            Container(
+              alignment: Alignment.center,
+              margin: EdgeInsets.symmetric(horizontal: 40),
+              child: TextField(
+                decoration: InputDecoration(
+                    labelText: "Name"
+                ),
+              ),
+            ),
+
+            SizedBox(height: size.height * 0.03),
+
+            Container(
+              alignment: Alignment.center,
+              margin: EdgeInsets.symmetric(horizontal: 40),
+              child: TextField(
+                decoration: InputDecoration(
+                    labelText: "Mobile Number"
+                ),
+              ),
+            ),
+
+            SizedBox(height: size.height * 0.03),
+
+            Container(
+              alignment: Alignment.center,
+              margin: EdgeInsets.symmetric(horizontal: 40),
+              child: TextField(
+                decoration: InputDecoration(
+                    labelText: "Username"
+                ),
+              ),
+            ),
+
+            SizedBox(height: size.height * 0.03),
+
+            Container(
+              alignment: Alignment.center,
+              margin: EdgeInsets.symmetric(horizontal: 40),
+              child: TextField(
+                decoration: InputDecoration(
+                    labelText: "Password"
+                ),
+                obscureText: true,
+              ),
+            ),
+
+            SizedBox(height: size.height * 0.05),
+
+            Container(
+              alignment: Alignment.centerRight,
+              margin: EdgeInsets.symmetric(horizontal: 40, vertical: 10),
+              child: ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(80.0)
+                    ),
+                    primary: Color.fromARGB(255, 255, 136, 34),
+                    padding: const EdgeInsets.all(0),
+                    textStyle: TextStyle(
+                        fontWeight: FontWeight.bold
+                    ),
+                    minimumSize: Size(size.width * 0.5, 50.0)
+                ),
+                child: Text(
+                  "SIGN UP",
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            ),
+
+            Container(
+              alignment: Alignment.centerRight,
+              margin: EdgeInsets.symmetric(horizontal: 40, vertical: 10),
+              child: GestureDetector(
+                onTap: () => {
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => LoginScreen()))
+                },
+                child: Text(
+                  "Already Have an Account? Sign in",
+                  style: TextStyle(
+                      fontSize: 12,
+                      fontWeight: FontWeight.bold,
+                      color: Color(0xFF2661FA)
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
