7f9a89d0ef5e6cd7fbe05d8dbc3d278c57e748ae
Dushantha Olinda <2020cs185@stu.ucsc.cmb.ac.lk>
2023-03-04 10:55:00 +0530

    Sidebar added

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:madhack_finals/screens/home/home.dart';
 import 'firebase_options.dart';
 
 import 'package:madhack_finals/constants.dart';
@@ -24,7 +25,7 @@ class MyApp extends StatelessWidget {
         primaryColorLight: primaryLight,
         scaffoldBackgroundColor: Colors.white
       ),
-      home: const WelcomeScreen(),
+      home: const Home2(),
     );
   }
 }
