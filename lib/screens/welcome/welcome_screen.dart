76e84096c2541f164d5b82c52ffede1133ba135a
Dushantha Olinda <2020cs185@stu.ucsc.cmb.ac.lk>
2023-03-04 17:47:57 +0530

    removed unnecessary import

--- lib/screens/welcome/welcome_screen.dart
+++ lib/screens/welcome/welcome_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:madhack_finals/constants.dart';
 import 'package:madhack_finals/screens/welcome/components/body.dart';
 
 class WelcomeScreen extends StatelessWidget {
