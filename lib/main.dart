89b4424d80af148f789523a59621df2521a425ad
thilankas961 <thilankas961@gmail.com>
2023-03-04 10:33:32 +0530

    firebase connected

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,9 @@ import 'package:madhack_finals/constants.dart';
 import 'package:madhack_finals/screens/welcome/welcome_screen.dart';
 
 
-void main() {
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
